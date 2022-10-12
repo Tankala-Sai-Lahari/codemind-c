e5480d14a7faf39dd6d30343edab2cd247f09e4f
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-10-12 23:20:45 +0530

    #include<stdio.h> int main() {     int n,sum=0,r,temp,i;     scanf("%d",&n);     temp=n;     for(i=1;i<n;i++)     {         if(n%i==0)         sum=sum+i;     }     if(n==sum)     {         printf("True");     }     else     printf("False");     return 0; }

---
+++ Perfect_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,r,temp,i;
+    scanf("%d",&n);
+    temp=n;
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        sum=sum+i;
+    }
+    if(n==sum)
+    {
+        printf("True");
+    }
+    else
+    printf("False");
+    return 0;
+}
